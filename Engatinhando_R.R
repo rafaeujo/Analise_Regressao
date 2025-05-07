bb974531467b18d56cda88742854100b83a78d3e
rafaeujo <152826349+rafaeujo@users.noreply.github.com>
2025-05-07 09:10:09 -0300

    Update Engatinhando_R.R
    AULA 2 R

--- Engatinhando_R.R
+++ Engatinhando_R.R
@@ -8,7 +8,7 @@ set.seed(1234)
 #Definindo parâmetros e variáveis
 
 REP <- 10000
-n <- 10
+n <- 50
 
 beta0 <- 3
 beta1 <- 5
@@ -34,4 +34,46 @@ hist(repli[2,], main = "Histograma de beta1")
 
 #Testando a normalidade dos erros
 
-qqPlot(erro, envelope = 0.99)
+par(mfrow = c(1,2))
+qqPlot(repli[1,], envelope = 0.95)
+qqPlot(repli[2,], envelope = 0.95)
+
+#Mudando a distribuição dos erros
+
+beta0_T <- 3
+beta1_T <- 5
+
+Simulando_T <- function(){
+  erro2 <- rt(n, df = 3)
+  x <- runif(n)
+  y <- beta0_T + beta1_T*x + erro2
+  fit <- lm(y~x)
+  return(fit$coefficients)
+}
+
+repli_T <- replicate(REP, Simulando_T())
+
+par(mfrow = c(2,2))
+qqPlot(repli_T[1,], envelope = 0.95)
+qqPlot(repli_T[2,], envelope = 0.95)
+qqPlot(repli[1,], envelope = 0.95)
+qqPlot(repli[2,], envelope = 0.95)
+
+var(repli_T[1,])
+var(repli_T[2,])
+
+#Teorema Central do Limite
+
+Cauchy <- function(){
+  n2 <-  1000
+  x_cauchy <-  rcauchy(n2)
+  return(mean(x_cauchy))
+}
+par(mfrow  = c(1,1))
+repli2 <- replicate(REP, Cauchy())
+qqPlot(repli2, distribution = 't', df = 1)
+hist(repli2)
+
+# Tarefa: Criar uma função que receba um vetor de letras e retorne cada letra e o número que estará associado à letra
+
+~/Rafael Jordane/Analise_Regressao
