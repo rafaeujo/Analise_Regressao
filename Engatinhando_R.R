7603de583cd842498b7973484726df77914f8895
rafaeujo <rafael.jordane.2003@.gmail.com>
2025-05-20 23:29:18 -0300

    abluble

--- Engatinhando_R.R
+++ Engatinhando_R.R
@@ -74,6 +74,71 @@ repli2 <- replicate(REP, Cauchy())
 qqPlot(repli2, distribution = 't', df = 1)
 hist(repli2)
 
-# Tarefa: Criar uma função que receba um vetor de letras e retorne cada letra e o número que estará associado à letra
+# Função modificada para testar consistência
+Simulando_consistencia <- function(n) {
+  erro <- rnorm(n)
+  x <- runif(n)
+  y <- beta0 + beta1*x + erro
+  fit <- lm(y~x)
+  return(fit$coefficients)
+}
 
-~/Rafael Jordane/Analise_Regressao
+# Testando para diferentes tamanhos de amostra
+tamanhos <- c(50, 100, 500, 1000, 5000)
+resultados <- list()
+
+for (i in seq_along(tamanhos)) {
+  repli <- replicate(REP, Simulando_consistencia(tamanhos[i]))
+  resultados[[i]] <- list(
+    n = tamanhos[i],
+    media_beta0 = mean(repli[1,]),
+    media_beta1 = mean(repli[2,]),
+    var_beta0 = var(repli[1,]),
+    var_beta1 = var(repli[2,])
+  )
+}
+
+# Exibindo os resultados
+for (res in resultados) {
+  cat("\nTamanho da amostra:", res$n,
+      "\nMédia de beta0:", res$media_beta0,
+      "\nMédia de beta1:", res$media_beta1,
+      "\nVariância de beta0:", res$var_beta0,
+      "\nVariância de beta1:", res$var_beta1, "\n")
+}
+
+# Função de simulação com erros t-Student
+Simulando_t <- function(n) {
+  erro <- rt(n, df = 3)  # Erros com distribuição t(3)
+  x <- runif(n)          # Variável explicativa uniforme
+  y <- beta0 + beta1*x + erro
+  fit <- lm(y ~ x)
+  return(fit$coefficients)
+}
+
+# Tamanhos de amostra para testar consistência
+tamanhos <- c(50, 100, 500, 1000, 5000)
+resultados <- list()
+
+# Executando a simulação para cada tamanho de amostra
+for (i in seq_along(tamanhos)) {
+  repli <- replicate(REP, Simulando_t(tamanhos[i]))
+  resultados[[i]] <- list(
+    n = tamanhos[i],
+    media_beta0 = mean(repli[1,]),
+    media_beta1 = mean(repli[2,]),
+    var_beta0 = var(repli[1,]),
+    var_beta1 = var(repli[2,])
+  )
+}
+
+# Exibindo os resultados
+for (res in resultados) {
+  cat("\nTamanho da amostra:", res$n,
+      "\nMédia de beta0:", round(res$media_beta0, 4),
+      "\nMédia de beta1:", round(res$media_beta1, 4),
+      "\nVariância de beta0:", round(res$var_beta0, 6),
+      "\nVariância de beta1:", round(res$var_beta1, 6), "\n")
+}
+
+correlacao_empirica <- cor(t(repli)[,1], t(repli)[,2])
